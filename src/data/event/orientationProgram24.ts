ba54c5f2f13d805301b198504174d346cfc0811a
Deviators Club <clubdeviators@gmail.com>
2025-08-09 01:40:33 +0530

    Update orientationProgram24.ts

--- src/data/event/orientationProgram24.ts
+++ src/data/event/orientationProgram24.ts
@@ -7,7 +7,7 @@ import Image4 from "@/assets/events/orientationProgram24/4.jpeg";
 import Image5 from "@/assets/events/orientationProgram24/5.png";
 
 export const OrientationProgram24: Omit<EVENT, "slug"> = {
-  title: "Orientation Program",
+  title: "Orientation 2024",
   // 25 aug 2024
   date: new Date("2024-08-25T18:25:00.000Z"),
   images: [Image1, Image3, Image4, Image5, Image2],
