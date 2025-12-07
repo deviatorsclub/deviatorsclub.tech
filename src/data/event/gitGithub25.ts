feaea85f9ba8311a5493e6d403f8fcb08f7a8d94
damnbhavya <eternallybhavya@gmail.com>
2025-12-08 00:49:13 +0530

    chore: update

--- src/data/event/gitGithub25.ts
+++ src/data/event/gitGithub25.ts
@@ -1,15 +1,15 @@
 import { EVENT } from "@/types/event";
 
-import Image1 from "@/assets/events/gitGithub25/1.png";
-import Image2 from "@/assets/events/gitGithub25/2.png";
-import Image3 from "@/assets/events/gitGithub25/3.png";
-import Image4 from "@/assets/events/gitGithub25/4.png";
-import Image5 from "@/assets/events/gitGithub25/5.png";
-import Image6 from "@/assets/events/gitGithub25/6.png";
-import Image7 from "@/assets/events/gitGithub25/7.png";
-import Image8 from "@/assets/events/gitGithub25/8.png";
-import Image9 from "@/assets/events/gitGithub25/9.png";
-import Image10 from "@/assets/events/gitGithub25/10.png";
+import Image1 from "@/assets/events/gitGithub25/1.webp";
+import Image2 from "@/assets/events/gitGithub25/2.webp";
+import Image3 from "@/assets/events/gitGithub25/3.webp";
+import Image4 from "@/assets/events/gitGithub25/4.webp";
+import Image5 from "@/assets/events/gitGithub25/5.webp";
+import Image6 from "@/assets/events/gitGithub25/6.webp";
+import Image7 from "@/assets/events/gitGithub25/7.webp";
+import Image8 from "@/assets/events/gitGithub25/8.webp";
+import Image9 from "@/assets/events/gitGithub25/9.webp";
+import Image10 from "@/assets/events/gitGithub25/10.webp";
 export const GitGithub25: Omit<EVENT, "slug"> = {
   title: "Git & Github Session 2025",
   caption: `Back with another round of commits, clones, and collabs! 🔥
